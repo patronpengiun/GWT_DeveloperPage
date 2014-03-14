40b1b0ef91b9ffa5c84286717c89fe06f361e300
patronpenguin <xh5277@gmail.com>
2014-03-14 13:23:31 -0400

    V1

---
+++ DevPage/src/devPage/client/DevPage.java
@@ -0,0 +1,32 @@
+package devPage.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+public class DevPage implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		final TextBox usernameBox = new TextBox();
+
+
+		RootPanel.get("nameFieldContainer").add(usernameBox);
+		
+
+		
+	}
+}
