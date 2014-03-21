300178dda5cc6fe0f43d193732bfcff78f64f9cd
LiangFang <liangfang09@gmail.com>
2014-03-21 19:22:33 -0400

    Add game submit page, using GWT-Bootstrap, added gwt-bootstrap-2.3.2.0.jar

--- DevPage/src/devPage/client/DevPage.java
+++ DevPage/src/devPage/client/DevPage.java
@@ -10,8 +10,10 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -21,12 +23,14 @@ public class DevPage implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		final TextBox usernameBox = new TextBox();
-
-
-		RootPanel.get("nameFieldContainer").add(usernameBox);
+/*		final TextBox usernameBox = new TextBox();
+		PasswordTextBox normalText = new PasswordTextBox();
+		VerticalPanel panel = new VerticalPanel();
+		panel.add(normalText);
+		panel.add(usernameBox);
 		
-
-		
+		RootPanel.get("nameFieldContainer").add(panel);
+*/
+		RootPanel.get().add(new devPageUiBinder());
 	}
 }
