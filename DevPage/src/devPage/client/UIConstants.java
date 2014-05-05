15f6a8135ba38cb77c237bcf671c4dd9b2a82801
hj690 <hj690@nyu.edu>
2014-05-05 17:34:28 -0400

    add i18n to all UI string

---
+++ DevPage/src/devPage/client/UIConstants.java
@@ -0,0 +1,46 @@
+package devPage.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+
+public interface UIConstants extends Constants{
+
+	String MainPageTitle();
+	String GameConsole();
+	String SubmitGame();
+	String UpdateGame();
+	String DeleteGame();
+	String DashBoard();
+	String gameName();
+	String ifToken();
+	String turnBased();
+	String gameURL();
+	String ifAI();
+	String pic();
+	String gameDetail();
+	String SubmitSubHead();
+	String headUpd();
+	String subheadUpd();
+	String upDate();
+    String back();
+    String yes();
+    String no();
+    String playerNum();
+    String submisstionFail();
+    String submissionSuc();
+    String nameOccupied();
+    String inforMiss();
+    String validFail();
+    String updateFail();
+    String updateSuc();
+    String getInfoFail();
+    String noData();
+    String accessServerFail();
+    String noGame();
+    String deleteFail();
+    String cannotSendReq();
+    String loginFail();
+    String paserJSONfail();
+    String GameID();
+    String deleteSuc();
+}
